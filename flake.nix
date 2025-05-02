4233454eb3bf0e2970fa77fb8cb902e9a7bc6fb1
LeoArr <leopold.arrestrom@gmail.com>
2025-05-02 16:07:16 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Leo's NixOS conf";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, ... }@inputs: {
+    nixosConfigurations.t490 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+	inputs.home-manager.nixosModules.home-manager {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+
+            home-manager.users.leo = import ./home.nix;
+
+          }
+      ];
+    };
+  };
+}
